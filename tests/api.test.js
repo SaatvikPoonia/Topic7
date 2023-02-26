3959ef9a051d1fc92a054c780624477219eed36c
SaatvikPoonia <Saatvik4786.be21@chitkara.edu.in>
2023-02-27 01:20:02 +0530

    first commit

---
+++ tests/api.test.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+API_URL="http://localhost:5002/api"
+
+test('test device array', () => {
+    axios.get(`${API_URL}/devices`)
+      .then(resp => resp.data)
+      .then(resp => {
+        expect(resp[0].user).toEqual('not-a-user');
+      });
+    });
---
+++ web/server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+const base = `${__dirname}/public`;
+
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+    res.sendFile(`${base}/device-list.html`);
+  });
+
+  app.get('/iot-applications', function (req, res) {
+    res.sendFile(`${base}/iot-applications.html`);
+  });
+  app.get('/register-device', function (req, res) {
+    res.sendFile(`${base}/register-device.html`);
+  });
+
+  app.get('/send-command', (req, res) => {
+    res.sendFile(`${base}/send-command.html`);
+    });
+
+  app.get('*', (req, res) => {
+    res.sendFile(`${base}/404.html`);
+  });
+
+
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
+
+
+
+
